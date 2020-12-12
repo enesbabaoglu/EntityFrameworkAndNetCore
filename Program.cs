3e91b5b29d653131c9075b64c7b45db6c492f2d6
enesbabaoglu <enessbabaoglu@gmail.com>
2020-12-12 18:01:24 +0300

    added Shop db using code first method

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkAndNetCore
+{
+    public class ShopContext : DbContext{
+
+        DbSet<Product> Products { get; set; } 
+        DbSet<Category> Categories {get;set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=shop.db");
+
+    }
+
+    public class Product {
+         public int Id { get; set; } 
+        
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        
+        [Required]    
+        public double Price { get; set; }
+    }
+    public class Category {
+         public int Id { get; set; } 
+        
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+
+    class Program {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
